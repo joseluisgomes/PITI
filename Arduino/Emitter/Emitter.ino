32ee0d8b848fbd38963c6db81564387b7716777c
Rui Cunha <101945306+ruipCunha0@users.noreply.github.com>
2023-03-10 18:13:18 +0000

    Codigo atualizado capaz de enviar com sincronização de 8 bits.

--- Arduino/Emitter/Emitter.ino
+++ Arduino/Emitter/Emitter.ino
@@ -1,6 +1,7 @@
 // sender code for ESP32 board
 const int outputPin = 13; // set the digital output pin for transmitting the character
 char characterToSend = 'r'; // set the character to send
+const byte syncPattern = 0b10101010; // set the 8-bit synchronization pattern
 
 void setup() {
   Serial.begin(9600); // initialize serial communication for debugging
@@ -10,7 +11,13 @@ void setup() {
 void loop() {
   Serial.print("Sending character: ");
   Serial.println(characterToSend); // print the character to send to the Serial monitor
-  digitalWrite(outputPin, LOW); // set the output pin to LOW to signal the start of the character transmission
+  
+  // send the synchronization pattern to indicate the start of a character transmission
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(outputPin, bitRead(syncPattern, i)); // set the output pin to the i-th bit of the synchronization pattern
+    delay(10); // wait for 10 ms before transmitting the next bit
+  }
+
   delay(10); // wait for 10 ms before transmitting the first bit
   for (int i = 0; i < 8; i++) { // iterate over all 8 bits of the character
     if (bitRead(characterToSend, i) == 1) { // check if the i-th bit of the character is 1
@@ -21,5 +28,5 @@ void loop() {
     delay(10); // wait for 10 ms before transmitting the next bit
   }
   digitalWrite(outputPin, HIGH); // set the output pin to HIGH to signal the end of the character transmission
-  delay(5000); // wait for 1 second before sending the next character
+  delay(5000); // wait for 5 second before sending the next character
 }
--- Arduino/Receiver/Receiver.ino
+++ Arduino/Receiver/Receiver.ino
@@ -1,37 +1,51 @@
 // receiver code for ESP32 board
 const int inputPin = 4; // set the digital input pin for receiving the character
 char receivedChar = 'A'; // initialize a variable to store the received character
+const byte syncPattern = 0b10101010; // set the 8-bit synchronization pattern
 
 void setup() {
   Serial.begin(9600); // initialize serial communication for debugging
+  pinMode(inputPin, INPUT); // set the inputPin as an input
 }
 
 void loop() {
 
-  if (digitalRead(inputPin) == LOW) { // wait for the start bit
-
-    Serial.println("Receiving character...");
-    delay(10);
-    for (int i = 0; i < 8; i++) { // iterate over all 8 bits of the character
-
-      if (digitalRead(inputPin) == HIGH) { // check if the input pin is still HIGH
-        Serial.print("1");
-        bitSet(receivedChar, i); // set the i-th bit of the character to 1
-
-      } else if(digitalRead(inputPin) == LOW) {
-        Serial.print("0");
-        bitClear(receivedChar, i); // set the i-th bit of the character to 0
-        
+  // look for the synchronization pattern to indicate the start of a character transmission
+  while (true) {
+    bool syncFound = true;
+    for (int i = 0; i < 8; i++) {
+      if (digitalRead(inputPin) != bitRead(syncPattern, i)) { // check if the i-th bit of the synchronization pattern matches the input pin state
+        syncFound = false; // if not, breaks from cycle and repeats process
+        break;
       }
-      delay(10); // wait for 10 ms before reading the next bit
+      delay(10); // wait for 10 ms before checking the next bit
     }
-    Serial.println();
-    Serial.print("Received character: ");
-    Serial.println(receivedChar); // print the received character to the Serial monitor
-    delay(1000);
+    if (syncFound) { // if True, leaves while(true) cycle 
+      Serial.println("Sync");
+      break;
+    }
   }
-}
 
+  Serial.println("Receiving character...");
+  delay(10);  // delay of 10 ms
+  for (int i = 0; i < 8; i++) { // iterate over all 8 bits of the character
+
+    if (digitalRead(inputPin) == HIGH) { // check if the input pin is still HIGH
+      Serial.print("1");
+      bitSet(receivedChar, i); // set the i-th bit of the character to 1
+
+    } else if(digitalRead(inputPin) == LOW) {
+      Serial.print("0");
+      bitClear(receivedChar, i); // set the i-th bit of the character to 0
+        
+    }
+    delay(10); // wait for 10 ms before reading the next bit
+  }
+  Serial.println();
+  Serial.print("Received character: ");
+  Serial.println(receivedChar); // print the received character to the Serial monitor
+  delay(1000);
+}
 
 
 
